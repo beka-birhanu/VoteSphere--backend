25031d71c32d02573d0e24f259093a00081776ef
beka birhanu <beka.b.atomsa@gmail.com>
2024-01-06 20:54:42 +0300

    feat(auth): implement sign-in & refresh token DTOs
    These changes introduce two new DTOs for authentication:
    - src/auth/dtos/refreshTokenDto.dto.ts
    - src/auth/dtos/signInUserDto.dto.ts
    
    These DTOs are essential for handling sign-in requests and refreshing
    JWT tokens in our authentication system.

---
+++ src/auth/dtos/refreshTokenDto.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class RefreshTokenDto {
+  @ApiProperty({
+    description: 'Username for refreshing the token',
+    example: 'beka_birhanu',
+  })
+  @IsNotEmpty({ message: 'Username cannot be empty' })
+  userName: string;
+}
