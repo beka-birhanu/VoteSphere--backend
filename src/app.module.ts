167cb047c80bf12fbeb495c37397f589fb524d26
beka birhanu <beka.b.atomsa@gmail.com>
2024-01-06 21:19:19 +0300

    chore(app): update providers in app.module.ts
    This commit modifies the providers in the app.module.ts file:
    - src/app.module.ts
    
    The changes involve updating service providers to meet the current
    requirements of the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { INestApplication, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TeamsModule } from './teams/teams.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
