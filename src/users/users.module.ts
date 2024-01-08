69c9491295e85268e5b9c7a2f15bacb6058a619b
beka birhanu <beka.b.atomsa@gmail.com>
2024-01-08 23:29:26 +0300

    Refactor(users): Update module and service for SQLite integration
    **Changes:**
    - Modified users.module.ts to adapt to SQLite integration.
    - Updated users.service.ts to support data retrieval from the SQLite database.
    
    **Context:**
    This commit refactors the users module and service to align with the
    integration of SQLite for data storage, ensuring compatibility with the
    overall database transition.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/typeORM/entities/user';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   exports: [UsersService],
   controllers: [UserController],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,55 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUserDto.dto';
-import { promises } from 'dns';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/typeORM/entities/user';
+import * as bcrypt from 'bcrypt';
 
-export type User = {
-  username: string;
-  email: string;
-  password: string;
-  role: string;
-  blackList: any[];
-};
+// private users: User[] = [
+//   {
+//     username: 'beka',
+//     email: 'beka@gmail.com',
+//     password: '$2b$10$uzlJHEFlfsB2TKK0OWTLXu8IXHXs12SAeyeu3heHB.98ZrAQRf/LC',
+//     role: 'Admin',
+//     blackList: [],
+//   },
+//   {
+//     username: 'beka-birhanu',
+//     email: 'bekabirhanu@gmail.com',
+//     password: '$2b$10$uzlJHEFlfsB2TKK0OWTLXu8IXHXs12SAeyeu3heHB.98ZrAQRf/LC',
+//     role: 'user',
+//     blackList: [],
+//   },
+// ];
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [
-    {
-      username: 'beka',
-      email: 'beka@gmail.com',
-      password: '$2b$10$uzlJHEFlfsB2TKK0OWTLXu8IXHXs12SAeyeu3heHB.98ZrAQRf/LC',
-      role: 'Admin',
-      blackList: [],
-    },
-    {
-      username: 'beka-birhanu',
-      email: 'bekabirhanu@gmail.com',
-      password: '$2b$10$uzlJHEFlfsB2TKK0OWTLXu8IXHXs12SAeyeu3heHB.98ZrAQRf/LC',
-      role: 'user',
-      blackList: [],
-    },
-  ];
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(
-      (user) => user.username === username || user.email === username,
-    );
+  async findOne(usernameOrEmail: string): Promise<User | undefined> {
+    return this.userRepository.findOne({
+      where: [{ username: usernameOrEmail }, { email: usernameOrEmail }],
+    });
   }
   async createUser(createUserDto: CreateUserDto) {
-    const newUser: User = { ...createUserDto, blackList: [] };
+    const salt = await bcrypt.genSalt(10);
+    createUserDto.password = await bcrypt.hash(createUserDto.password, salt);
+    const newUser: User = this.userRepository.create({
+      ...createUserDto,
+      tokenBlackList: null,
+    });
 
-    this.users.push(newUser);
+    await this.userRepository.save(newUser);
   }
 
   async getUserRole(username: string): Promise<string | undefined> {
     const user = await this.findOne(username);
-    return user.role;
+    return user?.role;
   }
-  async addBlackListToken(username: string, token: string) {
+
+  async addBlackListToken(username: string, token: string): Promise<void> {
     const user = await this.findOne(username);
-    user.blackList.push(token);
+
+    if (!user) {
+      throw new NotFoundException(
+        `User with username '${username}' not found.`,
+      );
+    }
+
+    if (!user.tokenBlackList) {
+      user.tokenBlackList = [];
+    }
+
+    user.tokenBlackList.push(token);
+    await this.userRepository.save(user);
   }
   async getBlacklist(username: string): Promise<string[]> {
     const user = await this.findOne(username);
-    return user.blackList;
+    return user ? user.tokenBlackList : [];
   }
 }
