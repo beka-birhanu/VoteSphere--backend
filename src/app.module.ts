9c100e7be2f3c4af017e5049061b2b1f4291b497
beka birhanu <beka.b.atomsa@gmail.com>
2024-01-10 22:43:12 +0300

    chore: Update package dependencies and project configuration
    Changes:
    - Modified: package.json
    - Modified: src/app.module.ts
    - Modified: yarn.lock
    
    Context:
    This commit includes updates to the project configuration and
    dependencies. The changes in the `package.json` file indicate
     modifications to the project's package dependencies, while the
     adjustments in `src/app.module.ts` involve changes to the
     application's main module. The `yarn.lock` file reflects the updated
     dependency tree.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,24 @@
-import { INestApplication, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { TeamsModule } from './teams/teams.module';
+import { GroupModule } from './group/group.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './typeORM/entities/user';
+import { Group } from './typeORM/entities/group';
+import { Poll } from './typeORM/entities/poll';
+import { PollOption } from './typeORM/entities/polloption';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
-    TeamsModule,
+    GroupModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'database/database.sqlite',
-      entities: [User],
+      entities: [User, Group, PollOption, Poll],
       synchronize: true,
     }),
   ],
