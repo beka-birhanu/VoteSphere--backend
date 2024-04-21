c709ca3fc1584f51d8dd95f83bc89136876f75d7
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-04-21 15:28:29 +0300

    add examples to group DTOs

--- src/group/dtos/getGroupDto.dto.ts
+++ src/group/dtos/getGroupDto.dto.ts
@@ -1,15 +1,19 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetGroupDto {
   @IsNotEmpty({ message: 'Admins username is required' })
-  @IsString({ message: 'username must be a string' })
+  @IsString({ message: 'Username must be a string' })
+  @ApiProperty({ example: 'beka_birhanu', description: 'Admin username' })
   adminUsername: string;
 
   @IsNotEmpty({ message: 'Group name cannot be empty' })
   @IsString({ message: 'Group name must be a string' })
+  @ApiProperty({ example: 'My Group', description: 'Name of the group' })
   groupName: string;
 
   @IsNotEmpty()
-  @IsNumber()
-  groupId: number;
+  @IsString()
+  @ApiProperty({ example: '12345hj6', description: 'ID of the group' })
+  groupId: string;
 }
