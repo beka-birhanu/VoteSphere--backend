231d147bb4f664439cf79c284d4ae6ec22d6bd05
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-04-21 15:33:53 +0300

    move membership controller logic to group controller

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,95 +1,185 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  UseGuards,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
-import {
-  ApiTags,
-  ApiResponse,
-  ApiBearerAuth,
-  ApiOperation,
-  ApiOkResponse,
-} from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post, UseGuards, Req, NotFoundException, Delete, UnauthorizedException, ConflictException, BadRequestException } from '@nestjs/common';
+import { ApiTags, ApiResponse, ApiBearerAuth, ApiOperation, ApiOkResponse, ApiCreatedResponse, ApiBody } from '@nestjs/swagger';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { JwtGuard } from 'src/auth/guards/jwtAuth.guard';
 import { RolesGuard } from 'src/auth/guards/role.guard';
 import { CreateGroupDto } from './dtos/createGroupDto.dto';
 import { GroupService } from './group.service';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { GetGroupDto } from './dtos/getGroupDto.dto';
+import { Request } from 'express';
 
 @ApiTags('groups')
 @Controller('groups')
 export class GroupController {
-  constructor(private groupService: GroupService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+    private readonly groupService: GroupService,
+  ) {}
 
+  /**
+   * Create a new group.
+   * @param createGroupDto - Group's information
+   * @returns {Object} - Object containing the created group details
+   * @throws {NotFoundException} if the requesting user does not exist
+   * @throws {ConflictException} if the requesting admin already has a group
+   * @throws {UnauthorizedException} if the user is not authorized to perform the operation
+   */
   @UseGuards(RolesGuard, JwtGuard)
   @Roles(['Admin'])
   @Post()
-  @ApiOperation({
-    summary: 'Create Group',
-    description: 'Create a new group. Requires Admin role.',
-  })
-  @ApiResponse({
-    status: 201,
-    description: 'Group created successfully.',
-    type: CreateGroupDto,
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Bad Request',
-  })
   @ApiBearerAuth()
-  async createGroup(@Body() createGroupDto: CreateGroupDto) {
-    try {
-      // Create a new group
-      const newGroup = await this.groupService.createGroup(createGroupDto);
+  @ApiOperation({ summary: 'Create Group', description: 'Create a new group. Requires Admin role.' })
+  @ApiBody({ type: CreateGroupDto, description: 'Group information including admin username and group name.' })
+  @ApiCreatedResponse({ description: 'Group created successfully.', type: GetGroupDto })
+  @ApiResponse({ status: 401, description: 'Unauthorized: User lacks necessary permissions.' })
+  @ApiResponse({ status: 404, description: 'Not Found: The requesting user does not exist.' })
+  @ApiResponse({ status: 409, description: 'Conflict: The requesting admin already has a group.' })
+  // Create a new group
+  async createGroup(@Req() request: Request, @Body() createGroupDto: CreateGroupDto): Promise<GetGroupDto> {
+    const token = request.headers.authorization.split(' ')[1];
+    const header_adminUsername = this.jwtService.verify(token, {
+      secret: `${process.env.JWT_SECRET}`,
+    }).username;
 
-      return {
-        success: true,
-        message: 'Group created successfully!',
-        data: newGroup,
-      };
-    } catch (error) {
-      throw new HttpException(
-        {
-          success: false,
-          message:
-            error.message || 'An error occurred while creating the group.',
-        },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+    // if username in token and body don't match throw unauthorized error
+    if (createGroupDto.adminUsername !== header_adminUsername) {
+      throw new UnauthorizedException('User lacks necessary permissions');
     }
+
+    return this.groupService.createGroup(createGroupDto);
   }
 
-  @Roles(['Admin', 'user'])
+  /**
+   * Get members of a group.
+   * @param groupId - ID of the group
+   * @returns {{username, email}[]} - Array containing the list of members
+   * @throws {NotFoundException} if the provided group ID is invalid
+   */
+  @Roles(['Admin', 'User'])
   @Get(':groupId/members')
-  @ApiOperation({
-    summary: 'Get Members',
-    description: 'Get members of a group. Requires Admin or User role.',
-  })
-  @ApiResponse({
-    status: 200,
-    description: 'Returns the list of members.',
-    isArray: true,
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Bad Request',
-  })
+  @ApiOperation({ summary: 'Get Members', description: 'Get members of a group. Requires Admin or User role.' })
   @ApiOkResponse({
     description: 'Returns the list of members.',
+    schema: { type: 'array', items: { type: 'object', properties: { username: { type: 'string' }, email: { type: 'string' } } } },
   })
-  async getMembers(@Param('groupId') groupId: number) {
-    const group = await this.groupService.findById(groupId);
+  @ApiResponse({ status: 404, description: 'Not Found: The provided group ID is invalid.' })
+  // get members
+  async getMembers(@Param('groupId') groupId: string): Promise<{ username: string; email: string }[]> {
+    const group = await this.groupService.findGroupById(groupId);
+    // check the user is member of the group(not implemented)
     if (!group) {
-      throw new BadRequestException('Invalid group Id');
+      throw new NotFoundException('Invalid group ID');
     }
-    const members = await this.groupService.findMembers(groupId);
-    return members;
+
+    return this.groupService.findMembers(groupId);
+  }
+
+  /**
+   * Add Member to Group.
+   * @param username - The username of the user to be added
+   * @param groupId - ID of the group
+   * @param request - The request object
+   * @returns {string} - Message indicating success
+   * @throws {NotFoundException} if the provided username is invalid
+   * @throws {UnauthorizedException} if the current user is not the admin of the requested group
+   */
+  @UseGuards(RolesGuard, JwtGuard)
+  @Roles(['Admin'])
+  @Post(':groupId/members')
+  @ApiOperation({ summary: 'Add Member to Group', description: 'Add a user to a group. Requires Admin role.' })
+  @ApiBearerAuth()
+  @ApiBody({ schema: { properties: { username: { type: 'string', description: 'The username of the user.', example: 'beka_birhanu' } } } })
+  @ApiResponse({ status: 200, description: 'Successful operation' })
+  @ApiResponse({ status: 401, description: 'Unauthorized: Current user is not admin for the requested group' })
+  @ApiResponse({ status: 404, description: 'Not Found: Invalid username or admin must create a group before attempting to add members' })
+  // add a member
+  async addMemberToGroup(@Body('username') username: string, @Param('groupId') groupId: string, @Req() request: Request): Promise<string> {
+    const authorizationHeader = request.headers['authorization'] as string;
+    const token = authorizationHeader.split(' ')[1];
+
+    const adminUsername = this.jwtService.verify(token, {
+      secret: `${process.env.JWT_SECRET}`,
+    }).username;
+
+    const group = await this.groupService.findGroupByAdminUsername(adminUsername);
+
+    if (!group) {
+      throw new NotFoundException('Admins must create a group before attempting to add members');
+    }
+
+    if (group.id !== groupId) {
+      throw new UnauthorizedException('current user is not admin for the requested group');
+    }
+
+    const user = await this.usersService.findOneByUsername(username);
+
+    if (!user) {
+      throw new NotFoundException('Invalid username');
+    }
+
+    if (user.role === 'Admin') {
+      throw new ConflictException('There can only be one admin per group');
+    }
+
+    const userHasGroup = await this.usersService.userHasGroup(username);
+
+    if (userHasGroup) {
+      throw new ConflictException(`User '${user.username}' already belongs to a group.`);
+    }
+
+    return this.groupService.addMemberToGroup(user, group);
+  }
+
+  /**
+   * Remove Member from Group.
+   * @param username - The username of the user to be removed
+   * @param groupId - ID of the group
+   * @param request - The request object
+   * @returns {string} - Message indicating success
+   * @throws {NotFoundException} if the provided username is invalid
+   * @throws {UnauthorizedException} if the user doesn't have permission for the requested group
+   */
+  @UseGuards(RolesGuard, JwtGuard)
+  @Roles(['Admin'])
+  @Delete(':groupId/members')
+  @ApiOperation({ summary: 'Remove Member from Group', description: 'Remove a user from a group. Requires Admin role.' })
+  @ApiBearerAuth()
+  @ApiBody({ schema: { properties: { username: { type: 'string', description: 'The username of the user.', example: 'beka_birhanu' } } } })
+  @ApiResponse({ status: 200, description: 'Successful operation' })
+  @ApiResponse({ status: 401, description: 'Unauthorized: User does not have permission for the requested group' })
+  @ApiResponse({ status: 404, description: 'Not Found: Invalid username or admin must create a group before attempting to remove members' })
+  // remove a member
+  async removeMemberFromGroup(@Body('username') username: string, @Param('groupId') groupId: string, @Req() request: Request): Promise<string> {
+    const authorizationHeader = request.headers['authorization'] as string;
+    const token = authorizationHeader.split(' ')[1];
+
+    const adminUsername = this.jwtService.verify(token, {
+      secret: `${process.env.JWT_SECRET}`,
+    }).username;
+
+    const group = await this.groupService.findGroupByAdminUsername(adminUsername);
+
+    if (!group) {
+      throw new NotFoundException('Admins must create a group before attempting to remove members');
+    }
+
+    if (group.id != groupId) {
+      throw new UnauthorizedException('user dont have permission for the requested group');
+    }
+
+    const user = await this.usersService.findOneByUsernameWithGroup(username);
+
+    if (!user) {
+      throw new NotFoundException('Invalid username');
+    }
+
+    if (user.role === 'Admin') {
+      throw new BadRequestException('Admins can not leave there own group');
+    }
+
+    return await this.groupService.removeMemberFromGroup(user, group);
   }
 }
--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -1,14 +1,12 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Group } from './../typeORM/entities/group';
 import { CreateGroupDto } from './dtos/createGroupDto.dto';
 import { UsersService } from 'src/users/users.service';
 import { GetGroupDto } from './dtos/getGroupDto.dto';
+import { User } from 'src/typeORM/entities/user';
+import { STATUS_CODES } from 'http';
 
 @Injectable()
 export class GroupService {
@@ -19,70 +17,109 @@ export class GroupService {
   ) {}
 
   async createGroup(createGroupDto: CreateGroupDto): Promise<GetGroupDto> {
-    const adminUser = await this.usersService.findOne(
-      createGroupDto.adminUsername,
-    );
+    // Find the admin user based on the provided username
+    const adminUser = await this.usersService.findOneByUsername(createGroupDto.adminUsername);
 
+    // If admin user doesn't exist, throw NotFoundException
     if (!adminUser) {
-      throw new NotFoundException(
-        `User with username ${createGroupDto.adminUsername} not found.`,
-      );
-    }
-    const existingGroup = await this.findByAdminUsername(
-      createGroupDto.adminUsername,
-    );
-    if (existingGroup) {
-      throw new BadRequestException('Admin can create only one group.');
+      throw new NotFoundException(`User with username ${createGroupDto.adminUsername} not found.`);
     }
 
+    // Check if the admin already has a group, if yes, throw ConflictException
+    const existingGroup = await this.findGroupByAdminUsername(createGroupDto.adminUsername);
+    if (existingGroup) {
+      throw new ConflictException('Admin can create only one group.');
+    }
+
+    // Create a new Group instance
     const group = new Group();
     group.groupName = createGroupDto.groupName;
-
     group.admin = adminUser;
 
+    // Save the new group to the database
     const newGroup = await this.groupRepository.save(group);
+
+    // Update the admin user's group association
     adminUser.group = newGroup;
     this.usersService.updateUser(adminUser);
+
+    // If the group is successfully created, construct and return the group DTO
     if (newGroup) {
       const groupDto = {
         groupId: group.id,
         groupName: group.groupName,
         adminUsername: group.admin.username,
       };
+
       return groupDto;
+    } else {
+      // If there is an error during group creation, throw InternalServerErrorException
+      throw new InternalServerErrorException();
     }
   }
 
-  async findByAdminUsername(adminUsername: string): Promise<Group | undefined> {
+  async findMembers(groupId: string): Promise<{ username: string; email: string }[]> {
+    // Call the usersService to retrieve users belonging to the specified group ID
+    return this.usersService.getUsersByGroupId(groupId);
+  }
+
+  async addMemberToGroup(user: User, group: Group) {
+    // Update the user's group association
+    user.group = group;
+
     try {
-      const group = await this.groupRepository.findOne({
-        where: { admin: { username: adminUsername } },
-      });
-
-      if (!group) {
-        return null;
-      }
-
-      return group;
+      // Save the updated user entity
+      await this.usersService.updateUser(user);
+      // Return a success message or status code
+      return STATUS_CODES.successful;
     } catch (error) {
-      return null;
+      // If an error occurs during the update, throw a BadRequestException
+      throw new BadRequestException(error.message || 'Bad Request');
     }
   }
-  async findById(groupId: number) {
+
+  async removeMemberFromGroup(user: User, group: Group) {
+    // Check if the user is a member of the provided group
+    if (!user.group || user.group.id != group.id) {
+      throw new BadRequestException(`User '${user.username}' is not a member of your group`);
+    }
+
+    // Remove the user's group association
+    user.group = null;
+
     try {
-      const group = await this.groupRepository.findOne({
-        where: { id: groupId },
-      });
-      if (!group) {
-        return null;
-      }
-
-      return group;
+      // Save the updated user entity
+      await this.usersService.updateUser(user);
+      // Return a success message or status code
+      return STATUS_CODES.successful;
     } catch (error) {
-      return null;
+      // If an error occurs during the update, throw a BadRequestException
+      throw new BadRequestException(error.message || 'Bad Request');
     }
   }
-  async findMembers(groupId: number) {
-    return await this.usersService.getUsersByGroupId(groupId);
+
+  async findGroupByAdminUsername(adminUsername: string): Promise<Group | undefined> {
+    // Use the groupRepository to find a group by admin username
+    return this.groupRepository.findOne({
+      where: { admin: { username: adminUsername } }, // Query based on admin username
+    });
+  }
+
+  async findGroupById(groupId: string) {
+    // Use the groupRepository to find a group by its ID
+    return this.groupRepository.findOne({
+      where: { id: groupId }, // Query based on group ID
+    });
+  }
+
+  async findGroupByIdWithAdmin(groupId: string) {
+    return this.groupRepository.findOne({
+      where: { id: groupId },
+      relations: ['admin_username'],
+    });
+  }
+
+  async deleteGroup(groupId: string) {
+    await this.groupRepository.delete(groupId);
   }
 }
