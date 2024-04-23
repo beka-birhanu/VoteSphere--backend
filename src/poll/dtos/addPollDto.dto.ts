1e72e4f68c04d8651c47a897df39801ddda66b0e
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-04-23 11:55:07 +0300

    refactor addPollDto
    added examples on fields and added groupID"

--- src/poll/dtos/addPollDto.dto.ts
+++ src/poll/dtos/addPollDto.dto.ts
@@ -1,11 +1,25 @@
-import { IsNotEmpty, IsObject, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 import { PollDto } from './pollDto.dto';
 
 export class AddPollDto {
+  @ApiProperty({ example: 'beka_birhanu', description: 'Admins username' })
   @IsNotEmpty({ message: 'Admins username is required' })
-  @IsString({ message: 'username must be a string' })
+  @IsString({ message: 'Username must be a string' })
   adminUsername: string;
 
-  @IsObject()
+  @ApiProperty({
+    example: { question: 'What is your favorite color?', options: ['Red', 'Blue', 'Green'] },
+    description: 'Poll object containing question and options',
+  })
+  @IsNotEmpty({ message: 'Sending a poll is required to add a poll' })
+  @ValidateNested()
+  @Type(() => PollDto)
   poll: PollDto;
+
+  @ApiProperty({ example: 'group123', description: 'GroupID' })
+  @IsNotEmpty({ message: 'GroupID is required' })
+  @IsString({ message: 'GroupID must be a string' })
+  groupID: string;
 }
