51a16ce9eb696b07328934a4ac84f01ae5b43953
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-16 22:36:22 +0300

    docs(group): remove unwanted comments

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -1,4 +1,11 @@
-import { BadRequestException, ConflictException, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Group } from './../typeORM/entities/group';
@@ -15,41 +22,28 @@ export class GroupService {
     private readonly usersService: UsersService,
   ) {}
 
-  /**
-   * Create a new group with the provided details.
-   * @param {CreateGroupDto} createGroupDto - The details of the group to be created.
-   * @returns {Promise<GetGroupDto>} - The DTO representing the newly created group.
-   * @throws {NotFoundException} - If the admin user does not exist.
-   * @throws {ConflictException} - If the admin already has a group.
-   * @throws {InternalServerErrorException} - If there is an error during group creation.
-   */
   async createGroup(createGroupDto: CreateGroupDto): Promise<GetGroupDto> {
-    // Find the admin user based on the provided username
-    const adminUser = await this.usersService.findOneByUsername(createGroupDto.adminUsername, true);
+    const loadGroup = true;
+    const adminUser = await this.usersService.findOneByUsername(createGroupDto.adminUsername, loadGroup);
 
-    // If admin user doesn't exist, throw NotFoundException
     if (!adminUser) {
       throw new NotFoundException(`User with username ${createGroupDto.adminUsername} not found.`);
     }
 
-    // Check if the admin already has a group, if yes, throw ConflictException
-    if (adminUser.group) {
+    const adminHasGroup = adminUser.group !== null;
+    if (adminHasGroup) {
       throw new ConflictException('Admin can create only one group.');
     }
 
-    // Create a new Group instance
     const group = new Group();
     group.groupName = createGroupDto.groupName;
     group.admin = adminUser;
 
-    // Save the new group to the database
     const newGroup = await this.groupRepository.save(group);
 
-    // Update the admin user's group association
     adminUser.group = newGroup;
     this.usersService.updateUser(adminUser);
 
-    // If the group is successfully created, construct and return the group DTO
     if (newGroup) {
       const groupDto = {
         groupID: group.id,
@@ -59,28 +53,19 @@ export class GroupService {
 
       return groupDto;
     } else {
-      // If there is an error during group creation, throw InternalServerErrorException
       throw new InternalServerErrorException();
     }
   }
 
-  /**
-   * Retrieve members of a group identified by the provided group ID.
-   * @param {string} groupId - The ID of the group whose members are to be retrieved.
-   * @returns {Promise<{ username: string; email: string; is_admin: boolean }[]>} - An array containing details of each member, including username, email, and whether they are an admin (true/false).
-   * @throws {NotFoundException} - If the group with the specified ID does not exist.
-   */
   async getMembers(groupId: string): Promise<{ username: string; email: string; is_admin: boolean }[]> {
     const group = await this.findOneById(groupId);
-    // check the user is member of the group(not implemented)
+
     if (!group) {
       throw new NotFoundException('Invalid group ID');
     }
 
-    // Call the usersService to retrieve users belonging to the specified group ID
     const members = await this.usersService.getUsersByGroupId(groupId);
 
-    // Transforming user elements based on their role
     const transformedMembers = members.map((user) => ({
       username: user.username,
       email: user.email,
@@ -90,30 +75,20 @@ export class GroupService {
     return transformedMembers;
   }
 
-  /**
-   * Add a new member to a group.
-   * @param {string} newMemberUsername - The username of the user to be added as a member.
-   * @param {string} adminUsername - The username of the admin who is adding the new member.
-   * @param {string} groupId - The ID of the group to which the new member is being added.
-   * @returns {Promise<boolean>} - A boolean indicating whether the operation was successful.
-   * @throws {NotFoundException} - If the admin or new member user does not exist.
-   * @throws {ConflictException} - If the admin does not have a group, the admin is not an admin for the specified group, the new member is already an admin, or the new member already belongs to a group.
-   */
   async addMemberToGroup(newMemberUsername: string, adminUsername: string, groupId: string): Promise<string> {
-    const admin = await this.usersService.findOneByUsername(adminUsername, true);
-    const group = admin.group;
+    const loadGroup = true;
+    const admin = await this.usersService.findOneByUsername(adminUsername, loadGroup);
+    const adminsGroup = admin.group;
 
-    // If the admin don't have a group
-    if (!group) {
+    if (!adminsGroup) {
       throw new NotFoundException('Admins must create a group before attempting to add members');
     }
 
-    // If the admins group and the requested group ID don't match deny access
-    if (group.id !== groupId) {
+    if (adminsGroup.id !== groupId) {
       throw new UnauthorizedException('current user is not admin for the requested group');
     }
 
-    const newMember = await this.usersService.findOneByUsername(newMemberUsername, true);
+    const newMember = await this.usersService.findOneByUsername(newMemberUsername, loadGroup);
 
     if (!newMember) {
       throw new NotFoundException('Invalid username');
@@ -128,93 +103,69 @@ export class GroupService {
     if (userHasGroup) {
       throw new ConflictException(`User '${newMember.username}' already belongs to a group.`);
     }
-    // Update the user's group association
-    newMember.group = group;
+
+    newMember.group = adminsGroup;
 
     const saveSuccess = await this.usersService.updateUser(newMember);
 
     return saveSuccess ? STATUS_CODES.successful : STATUS_CODES.InternalServerError;
   }
 
-  /**
-   * Remove a member from a group.
-   * @param {string} bannedMemberUsername - The username of the member to be removed.
-   * @param {string} adminUsername - The username of the admin who is removing the member.
-   * @param {string} groupId - The ID of the group from which the member is being removed.
-   * @returns {Promise<boolean>} - A boolean indicating whether the operation was successful.
-   * @throws {NotFoundException} - If the admin or banned member user does not exist.
-   * @throws {ConflictException} - If the admin does not have a group, the admin is not an admin for the specified group, the banned member is an admin, or the banned member is not a member of the provided group.
-   * @throws {BadRequestException} - If the banned member is not a member of the provided group.
-   */
   async removeMemberFromGroup(bannedMemberUsername: string, adminUsername: string, groupId: string): Promise<string> {
-    const admin = await this.usersService.findOneByUsername(adminUsername, true);
-    const group = admin.group;
+    const loadGroup = true;
+    const admin = await this.usersService.findOneByUsername(adminUsername, loadGroup);
+    const adminsGroup = admin.group;
 
-    // If the admin don't have a group
-    if (!group) {
+    if (!adminsGroup) {
       throw new NotFoundException('Admins must create a group before attempting to remove members');
     }
 
-    // If the admins group and the requested group ID don't match deny access
-    if (group.id !== groupId) {
+    if (adminsGroup.id !== groupId) {
       throw new UnauthorizedException('current user is not admin for the requested group');
     }
 
-    const bannedMember = await this.usersService.findOneByUsername(bannedMemberUsername, true);
+    const bannedMember = await this.usersService.findOneByUsername(bannedMemberUsername, loadGroup);
 
     if (!bannedMember) {
       throw new NotFoundException('Invalid member username');
     }
 
     if (bannedMember.role === 'Admin') {
-      throw new ConflictException('Admins can not leave there own group');
+      throw new ConflictException('Admins can not leave their own group');
     }
 
-    // Check if the user is a member of the provided group
-    if (!bannedMember.group || bannedMember.group.id != group.id) {
+    if (!bannedMember.group || bannedMember.group.id != adminsGroup.id) {
       throw new BadRequestException(`User '${bannedMember.username}' is not a member of your group`);
     }
 
-    // Remove the user's group association
     bannedMember.group = null;
     const saveSuccess = await this.usersService.updateUser(bannedMember);
 
     return saveSuccess ? STATUS_CODES.successful : STATUS_CODES.InternalServerError;
   }
 
-  /**
-   * Find a group based on the username of its admin.
-   * @param {string} adminUsername - The username of the admin associated with the group.
-   * @returns {Promise<Group | undefined>} - A Promise resolving to the group entity if found; otherwise, undefined.
-   */
   async findOneByAdminUsername(adminUsername: string): Promise<Group | undefined> {
-    // Use the groupRepository to find a group by admin username
     return this.groupRepository.findOne({
-      where: { admin: { username: adminUsername } }, // Query based on admin username
+      where: { admin: { username: adminUsername } },
     });
   }
 
-  /**
-   * Find a group by its ID.
-   * @param {string} groupId - The ID of the group to find.
-   * @returns {Promise<Group | undefined>} - A Promise resolving to the group entity if found; otherwise, undefined.
-   */
   async findOneById(groupId: string): Promise<Group | undefined> {
     return this.groupRepository.findOne({
-      where: { id: groupId }, // Query based on group ID
+      where: { id: groupId },
     });
   }
 
-  /**
-   * Checks if a user with the given username belongs to the specified group.
-   * @param {string} username - The username of the user to check.
-   * @param {string} groupId - The ID of the group to check membership against.
-   * @returns {Promise<boolean>} A promise that resolves to true if the user belongs to the group, or false otherwise.
-   */
   async belongsTo(username: string, groupId: string): Promise<boolean> {
-    const user = await this.usersService.findOneByUsername(username, true);
+    const loadGroup = true;
+    const user = await this.usersService.findOneByUsername(username, loadGroup);
 
-    // Check if the user exists and if the user's group ID matches the specified group ID
-    return !!user && user.group.id === groupId;
+    if (!user) {
+      return false;
+    }
+
+    const isMember = user.group.id === groupId;
+
+    return isMember;
   }
 }
