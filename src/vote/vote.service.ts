c94f1096110b7d31bb74767e14e03b31a4db99b7
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-05-28 22:42:09 +0300

    feat(vote): add vote service

---
+++ src/vote/vote.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Poll } from 'src/typeORM/entities/poll';
+import { PollOption } from 'src/typeORM/entities/polloption';
+import { User } from 'src/typeORM/entities/user';
+import { Vote } from 'src/typeORM/entities/vote';
+import { Repository } from 'typeorm';
+
+const UNIQUE_VIOLATION = '23505';
+const ALREADY_VOTED = 0;
+const SUCCESS = 1;
+
+@Injectable()
+export class VoteService {
+  constructor(
+    @InjectRepository(Vote)
+    private readonly voteRepository: Repository<Vote>,
+  ) {}
+
+  async addVote(poll: Poll, user: User, pollOption: PollOption) {
+    const newVote = this.voteRepository.create({ user, poll, pollOption });
+    try {
+      await this.voteRepository.save(newVote);
+
+      return SUCCESS;
+    } catch (error) {
+      if (error.code === UNIQUE_VIOLATION) {
+        return ALREADY_VOTED;
+      }
+    }
+  }
+
+  async findOneByUserAndPoll(poll: Poll, user: User) {
+    return this.voteRepository.findOne({ where: { poll, user } });
+  }
+}
